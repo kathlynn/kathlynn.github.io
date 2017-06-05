591c804e863496c6a50c186cc710edfd31485b77
kathlynn <klynnlui@gmail.com>
2017-06-05 15:13:35 +0930

    changed portfolio to tabs

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ particlesJS.load('particles-js', 'js/vendor/particlesjs-config.json', function()
   console.log('callback - particles.js config loaded');
 });
 
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
 
